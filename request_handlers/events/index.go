da3e3d406bc4b3bf901f401df5abae570ba2f1ac
adi surya <adisurya1@gmail.com>
2022-04-16 11:54:00 +0700

    event router

--- request_handlers/events/index.go
+++ request_handlers/events/index.go
@@ -3,9 +3,19 @@ package events
 import (
 	"net/http"
 
+	"github.com/adisurya/friendly-garbanzo/database/events"
+	"github.com/adisurya/friendly-garbanzo/structs/responses"
 	"github.com/labstack/echo/v4"
 )
 
 func Index(c echo.Context) error {
-	return c.String(http.StatusOK, "OK")
+	events, err := events.List()
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	res := responses.EventList{}
+	res.Events = events
+	return c.JSON(http.StatusOK, res)
 }
---
+++ structs/responses/event_list.go
@@ -0,0 +1,5 @@
+package responses
+
+type EventList struct {
+	Events interface{} `json:"events"`
+}
