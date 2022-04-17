8ff58c1c25d7f67292c8fb13991668240b37da39
adi surya <adisurya1@gmail.com>
2022-04-17 15:54:37 +0700

    create booking documentation

--- request_handlers/events/create.go
+++ request_handlers/events/create.go
@@ -14,7 +14,7 @@ import (
 // @Description Create event
 // @Tags Events
 // @param event body requests.CreateEvent true "Event Data"
-// @Success 200 {object} responses.ResponseId
+// @Success 201 {object} responses.ResponseId
 // @Failure 404,400,500 {object} responses.MyError
 // @Router /events [post]
 func Create(c echo.Context) error {
--- request_handlers/tickets/Book.go
+++ request_handlers/tickets/Book.go
@@ -8,6 +8,14 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// Book ... Booking tickets for an event
+// @Summary Booking tickets for an event
+// @Description Booking tickets for an event
+// @Tags Tickets
+// @param event body requests.CreateBooking true "Booking Data"
+// @Success 201 {object} bookings.CreateBookingResponse
+// @Failure 404,400,500 {object} responses.MyError
+// @Router /tickets/booking [post]
 func Book(c echo.Context) error {
 	b := new(requests.CreateBooking)
 	if err := c.Bind(b); err != nil {
